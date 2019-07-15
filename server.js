22742185d9b3050bb141983bb46eb8c97e18871d
anarossini <anarossini01@gmail.com>
2019-07-15 14:45:53 -0300

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
 
 const alunasController = require('./AlunasController')
 const professorasController = require('./ProfessorasController')
