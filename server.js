0c1f7823f0dbfe406cc078fee0d32a33ae6cdd57
anarossini <anarossini01@gmail.com>
2019-07-15 13:40:47 -0300

    server.js arrumado

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const controller = require('./AlunasController')
-const professorascontroller = require('./ProfessorasController')
+
+const alunasController = require('./AlunasController')
+const professorasController = require('./ProfessorasController')
 
 const servidor = express()
 servidor.use(cors())
 servidor.use(bodyParser.json())
 
 servidor.get('/alunas', async (request, response) => {
-  controller.getAll()
-    .then(alunas => response.send(alunas))
+  let alunas = await alunasController.getAll()
+
+  response.send(alunas)
 })
 
 servidor.get('/professoras', async (request, response) => {
-  let professoras = await professorascontroller.getAll()
+  let professoras = await professorasController.getAll()
 
-  response.status(200).send(professoras)
+  response.send(professoras)
 })
 
 servidor.post('/users', async (request, response) => {
-let users = await UserController.post()
+  let user = await UserController.post(request.body)
 
-response.status(200).send(users)
+  if (user){
+    response.status(201).send(user)
+  } else {
+    response.sendStatus(400)
+  }
 })
 
 servidor.listen(3000)
-console.log("servidorzinho rodando na porta 3000")
